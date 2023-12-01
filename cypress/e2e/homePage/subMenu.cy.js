f7c4ab6fe88442aa3a7e80c3ce1f824d56bcfc49
prastowoardi <prastowoardi616@gmail.com>
2023-12-01 18:49:55 +0700

    optimasi cek sub menu

--- cypress/e2e/homePage/subMenu.cy.js
+++ cypress/e2e/homePage/subMenu.cy.js
@@ -11,65 +11,33 @@ describe ('Cek fungsi dropdown menu sub menu pada halaman index', () => {
         }
     })
 
-    it ('Sub Menu: Jaket wanita', () => {
-        cy.topCewek()
-        cy.clickSubMenuAndCheckURL('#ui-id-11', 'Jackets', 'https://magento.softwaretestingboard.com/women/tops-women/jackets-women.html');
-    })
-
-    it ('Sub Menu: Hoodie wanita', () => {
-        cy.topCewek()
-        cy.clickSubMenuAndCheckURL('#ui-id-12', 'Hoodies & Sweatshirts', 'https://magento.softwaretestingboard.com/women/tops-women/hoodies-and-sweatshirts-women.html')
-    })
-
-    it ('Sub Menu: Tees wanita', () => {
-        cy.topCewek()
-        cy.clickSubMenuAndCheckURL('#ui-id-13', 'Tees', 'https://magento.softwaretestingboard.com/women/tops-women/tees-women.html')
-    })
-
-    it ('Sub Menu: Bra wanita', () => {
-        cy.topCewek()
-        cy.clickSubMenuAndCheckURL('#ui-id-14', 'Bras & Tanks', 'https://magento.softwaretestingboard.com/women/tops-women/tanks-women.html')
-    })
+    const testSubMenu = (category, subMenuType, subMenuId, subMenuText, expectedUrl) => {
+        it(`Sub Menu (${category}): ${subMenuText}`, () => {
+            if (category === 'women') {
+                cy[`${subMenuType}Cewek`]()
+            } else if (category === 'men') {
+                cy[`${subMenuType}Cowok`]()
+            }
     
-    it ('Sub Menu: Celana wanita', () => {
-        cy.bottomCewek()
-        cy.clickSubMenuAndCheckURL('#ui-id-15', 'Pants', 'https://magento.softwaretestingboard.com/women/bottoms-women/pants-women.html')
-    })
+            cy.clickSubMenuAndCheckURL(subMenuId, subMenuText, expectedUrl)
+        })
+    }
     
-    it ('Sub Menu: Short wanita', () => {
-        cy.bottomCewek()
-        cy.clickSubMenuAndCheckURL('#ui-id-16', 'Shorts', 'https://magento.softwaretestingboard.com/women/bottoms-women/shorts-women.html')
-    })
-
-    it ('Sub Menu: Jaket pria', () => {
-        cy.topCowok()
-        cy.clickSubMenuAndCheckURL('#ui-id-19', 'Jackets', 'https://magento.softwaretestingboard.com/men/tops-men/jackets-men.html')
-    })
-
-    it ('Sub Menu: Hoodie pria', () => {
-        cy.topCowok()
-        cy.clickSubMenuAndCheckURL('#ui-id-20', 'Hoodies & Sweatshirts', 'https://magento.softwaretestingboard.com/men/tops-men/hoodies-and-sweatshirts-men.html')
-    })
-
-    it ('Sub Menu: Tees pria', () => {
-        cy.topCowok()
-        cy.clickSubMenuAndCheckURL('#ui-id-21', 'Tees', 'https://magento.softwaretestingboard.com/men/tops-men/tees-men.html')
-    })
-
-    it ('Sub Menu: Tanks pria', () => {
-        cy.topCowok()
-        cy.clickSubMenuAndCheckURL('#ui-id-22', 'Tanks', 'https://magento.softwaretestingboard.com/men/tops-men/tanks-men.html')
-    })
-
-    it ('Sub Menu: Celana pria', () => {
-        cy.bottomCowok()
-        cy.clickSubMenuAndCheckURL("#ui-id-23", 'Pants','https://magento.softwaretestingboard.com/men/bottoms-men/pants-men.html')
-    })
-
-    it ('Sub Menu: Shorts pria', () => {
-        cy.bottomCowok()
-        cy.clickSubMenuAndCheckURL('#ui-id-24', 'Shorts','https://magento.softwaretestingboard.com/men/bottoms-men/shorts-men.html')
-    })
+    // Women Sub Menus
+    testSubMenu('women', 'top', '#ui-id-11', 'Jackets', 'https://magento.softwaretestingboard.com/women/tops-women/jackets-women.html')
+    testSubMenu('women', 'top', '#ui-id-12', 'Hoodies & Sweatshirts', 'https://magento.softwaretestingboard.com/women/tops-women/hoodies-and-sweatshirts-women.html')
+    testSubMenu('women', 'top', '#ui-id-13', 'Tees', 'https://magento.softwaretestingboard.com/women/tops-women/tees-women.html')
+    testSubMenu('women', 'top', '#ui-id-14', 'Bras & Tanks', 'https://magento.softwaretestingboard.com/women/tops-women/tanks-women.html')
+    testSubMenu('women', 'bottom', '#ui-id-15', 'Pants', 'https://magento.softwaretestingboard.com/women/bottoms-women/pants-women.html')
+    testSubMenu('women', 'bottom', '#ui-id-16', 'Shorts', 'https://magento.softwaretestingboard.com/women/bottoms-women/shorts-women.html')
+    
+    // Men Sub Menus
+    testSubMenu('men', 'top', '#ui-id-19', 'Jackets', 'https://magento.softwaretestingboard.com/men/tops-men/jackets-men.html')
+    testSubMenu('men', 'top', '#ui-id-20', 'Hoodies & Sweatshirts', 'https://magento.softwaretestingboard.com/men/tops-men/hoodies-and-sweatshirts-men.html')
+    testSubMenu('men', 'top', '#ui-id-21', 'Tees', 'https://magento.softwaretestingboard.com/men/tops-men/tees-men.html')
+    testSubMenu('men', 'top', '#ui-id-22', 'Tanks', 'https://magento.softwaretestingboard.com/men/tops-men/tanks-men.html')
+    testSubMenu('men', 'bottom', '#ui-id-23', 'Pants', 'https://magento.softwaretestingboard.com/men/bottoms-men/pants-men.html')
+    testSubMenu('men', 'bottom', '#ui-id-24', 'Shorts', 'https://magento.softwaretestingboard.com/men/bottoms-men/shorts-men.html')
 
     it ('Sub Menu: Gear - Bags', () => {
         cy.bag()
