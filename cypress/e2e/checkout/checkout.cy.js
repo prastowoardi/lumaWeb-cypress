e244ffd47426a99185eaa5e8c381b17402625fcf
sysadmin codingcollective <prastowoardi616@gmail.com>
2024-04-04 09:36:36 +0700

    update checkout page

--- cypress/e2e/checkout/checkout.cy.js
+++ cypress/e2e/checkout/checkout.cy.js
@@ -1,3 +1,4 @@
+const faker = require('@faker-js/faker')
 const tees = "Desiree Fitness Tee"
 
 describe ('Checkout barang', () => {
@@ -31,11 +32,7 @@ describe ('Checkout barang', () => {
                 }
             }
             cy.log('Nama produk yang ada di keranjang: ' + productName)
-        })
-
-        cy.get('#cart-391699-qty').invoke('attr', 'value').then((value) => {
-            cy.log('Jumlah barang: ' + value)
-        })          
+        })         
 
         cy.wait(2000)
         cy.get('strong > .price').invoke('text').then((text) => {
@@ -46,15 +43,21 @@ describe ('Checkout barang', () => {
         cy.get('.methods > :nth-child(1) > .action').click()
 
         cy.wait(7000)
-        // Mengisi Alamat Pengiriman
-        cy.get('[name="company"]').type('PT Testing Indonesia')
-        cy.get('[name="street[0]"]').type('Jl. Pegangsaan Timur VI')
-        cy.get('[name="city"]').type('Semarang')
-        cy.get('[name="region_id"]').select('Florida')
-        cy.get('[name="postcode"]').type('50521')
-        cy.get('[name="country_id"]').select('Indonesia')
-        cy.get('[name="telephone"]').type('8098731231233')
-        cy.get('#checkout-shipping-method-load').contains('$5.00').click()
+        cy.get('.shipping-address-item.not-selected-item .action-select-shipping-item').click()
+        // // Mengisi Alamat Pengiriman jika belum ada
+        // cy.get('.new-address-popup > .action').click()
+        // cy.get('[name="company"]').type(faker.faker.company.name())
+        // cy.get('[name="street[0]"]').type(faker.faker.location.streetAddress())
+        // cy.get('[name="city"]').type(faker.faker.location.city())
+        // cy.get('[name="region_id"]').select('Virginia')
+        // cy.get('[name="postcode"]').type(faker.faker.location.zipCode())
+        // cy.get('[name="country_id"]').select('Uganda')
+        // cy.get('[name="telephone"]').type(faker.faker.phone.number())
+        
+        // // if you dont want to save address just comment
+        // // cy.get('#shipping-save-in-address-book').click()
+        // cy.get('.modal-footer > .primary').contains('Ship here').click()
+
         cy.get('.button').contains('Next').click()
 
         cy.wait(1000)
@@ -70,7 +73,7 @@ describe ('Checkout barang', () => {
         cy.url().should('eq', 'https://magento.softwaretestingboard.com/checkout/onepage/success/')
     })
 
-    it ('Tidak ada barang di keranjang belanja', () => {
+    it.only ('Tidak ada barang di keranjang belanja', () => {
         cy.get('.showcart').click()
         // cy.get('#ui-id-1').should('be.visible')
         cy.get('.cart-empty').invoke('text').then((text) => {
@@ -98,10 +101,6 @@ describe ('Checkout barang', () => {
             cy.log('Nama produk yang ada di keranjang: ' + productName)
         })
 
-        cy.get('#cart-391699-qty').invoke('attr', 'value').then((value) => {
-            cy.log('Jumlah barang: ' + value)
-        })          
-
         cy.wait(2000)
         cy.get('strong > .price').invoke('text').then((text) => {
             const grandTotal = text.replace(/[^\d.]/g, '')
@@ -111,15 +110,22 @@ describe ('Checkout barang', () => {
         cy.get('.methods > :nth-child(1) > .action').click()
 
         cy.wait(7000)
-        // Mengisi Alamat Pengiriman
-        cy.get('[name="company"]').type('PT Testing Indonesia')
-        cy.get('[name="street[0]"]').type('Jl. Pegangsaan Timur VI')
-        cy.get('[name="city"]').type('Semarang')
-        cy.get('[name="region_id"]').select('Florida')
-        cy.get('[name="postcode"]').type('50521')
-        cy.get('[name="country_id"]').select('Indonesia')
-        cy.get('[name="telephone"]').type('8098731231233')
-        cy.get('#checkout-shipping-method-load').contains('$5.00').click()
+        cy.get('.shipping-address-item.not-selected-item .action-select-shipping-item').click()
+
+        // // Mengisi Alamat Pengiriman jika belum ada
+        // cy.get('.new-address-popup > .action').click()
+        // cy.get('[name="company"]').type(faker.faker.company.name())
+        // cy.get('[name="street[0]"]').type(faker.faker.location.streetAddress())
+        // cy.get('[name="city"]').type(faker.faker.location.city())
+        // cy.get('[name="region_id"]').select('Virginia')
+        // cy.get('[name="postcode"]').type(faker.faker.location.zipCode())
+        // cy.get('[name="country_id"]').select('Uganda')
+        // cy.get('[name="telephone"]').type(faker.faker.phone.number())
+        
+        // // if you dont want to save address just comment
+        // // cy.get('#shipping-save-in-address-book').click()
+        // cy.get('.modal-footer > .primary').contains('Ship here').click()
+
         cy.get('.button').contains('Next').click()
 
         cy.wait(1000)
